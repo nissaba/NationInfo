6225b90b378e65320de06c3df094aecc340a1e27
Pascale Beaulac <nissaba@gmail.com>
2025-12-13 14:00:29 -0500

    Initial Commit

---
+++ NationInfo/NationInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationInfoApp.swift
+//  NationInfo
+//
+//  Created by Pascale on 2025-12-13.
+//
+
+import SwiftUI
+
+@main
+struct NationInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NationInfoTests/NationInfoTests.swift
@@ -0,0 +1,16 @@
+//
+//  NationInfoTests.swift
+//  NationInfoTests
+//
+//  Created by Pascale on 2025-12-13.
+//
+
+import Testing
+
+struct NationInfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
